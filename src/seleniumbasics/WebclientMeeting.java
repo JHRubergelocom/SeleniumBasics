c04602b85e753a3381a38c3d57457c4b853bd527
Jan-Hendrik Ruberg <j-h.ruberg@elo.com>
2022-10-26 15:48:26 +0200

    src/seleniumbasics/WebclientMeeting.java

--- src/seleniumbasics/WebclientMeeting.java
+++ src/seleniumbasics/WebclientMeeting.java
@@ -12,40 +12,36 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 /**
  *
  * @author ruberg
  */
 public class WebclientMeeting {
-    static private final Integer interval = 5000;
+    static private final Integer interval = 10000;
     
     static WebDriver StartSolution() {
         ChromeDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));        
         driver.get("http://ruberg-meeting.dev.elo/ix-Solutions/plugin/de.elo.ix.plugin.proxy/web/");
         return driver;
     }
     
-    static void LoginSolution(WebDriver driver) {
+    static void LoginSolution(WebDriver driver) throws InterruptedException {
         driver.findElement(By.xpath("//*[@id=\"field-focustext-1020-inputEl\"]")).sendKeys("Administrator");
         driver.findElement(By.xpath("//*[@id=\"textfield-1021-inputEl\"]")).sendKeys("elo");
         driver.findElement(By.xpath("//*[@id=\"button-1023-btnIconEl\"]")).click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));
+        Thread.sleep(interval);                
     } 
     
     static void CreateMeetingBoard(WebDriver driver) throws InterruptedException {
         
         // start create meetingboard
         driver.findElement(By.xpath("//*[@id=\"tile-1013\"]")).click(); // Solutions
-        // driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));        
         Thread.sleep(interval);        
         
-        
         driver.findElement(By.xpath("//*[@id=\"button-1218-btnIconEl\"]")).click(); // Neu
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));
         driver.findElement(By.xpath("//*[@id=\"button-1280-btnIconEl\"]")).click(); // Meeting
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));
         
         WebElement elem = driver.findElement(By.xpath("//*[@id=\"ext-comp-1274-textEl\"]")); // Neues Meeting Board
         Thread.sleep(interval);
@@ -54,28 +50,54 @@ public class WebclientMeeting {
 
         // set elements
         WebElement actelem = driver.switchTo().activeElement();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));
-        
+        Thread.sleep(interval);        
         WebDriver frame = driver.switchTo().frame(actelem); 
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));
+        Thread.sleep(interval);
         
+        // Allgemein
         frame.findElement(By.name("IX_GRP_MEETING_BOARD_NAME")).sendKeys("Meetingboard1");
         frame.findElement(By.name("IX_GRP_MEETING_BOARD_CODE")).sendKeys("MB1");
-        frame.findElement(By.name("IX_GRP_MEETING_BOARD_MINUTE_TAKER")).sendKeys("Adrian Smith"+ Keys.TAB);
+        frame.findElement(By.name("IX_GRP_MEETING_BOARD_MINUTE_TAKER")).sendKeys("Bodo Kraft" + Keys.TAB);
         Thread.sleep(interval);
-       
-        frame.findElement(By.name("IX_DESC")).sendKeys("Test");        
+        frame.findElement(By.name("IX_MAP_MEETING_BOARD_ORGANIZER1")).clear();
+        frame.findElement(By.name("IX_MAP_MEETING_BOARD_ORGANIZER1")).sendKeys("Jan Eichner" + Keys.TAB);
         Thread.sleep(interval);
+        frame.findElement(By.name("JS_ADDLINE")).click();
+        frame.findElement(By.name("IX_MAP_MEETING_BOARD_ORGANIZER2")).sendKeys("Sandra Renz" + Keys.TAB);
+        Thread.sleep(interval);
+               
+        frame.findElement(By.name("IX_DESC")).sendKeys("Beschreibung Meetingboard1");    
+        Thread.sleep(interval);
+        
+        // Mitglieder 
+        
+        List<WebElement> listTabs =frame.findElements(By.xpath("/html/body/form/div/div[1]/ul/li"));
+        for (WebElement tab: listTabs) {
+            // System.out.println(tab.getText());
+            String tabText = tab.getText();
+            if (tabText.contentEquals("Mitglieder")) {
+                tab.click();
+                Thread.sleep(interval);                
+            }            
+        }
+        
+        // Themen
+        
+        // Benachrichtigungen
+        
+        // Einstellungwn
+        
+        
         
         // click OK
         List<WebElement> listNextButtons =frame.findElements(By.name("NEXTNODE"));
         for (WebElement buttonNext: listNextButtons) {
-            System.out.println(buttonNext.getText());
+            // System.out.println(buttonNext.getText());
             String buttonText = buttonNext.getText();
             if (buttonText.contentEquals("OK")) {
                 buttonNext.click();
+                Thread.sleep(interval);                                
             }
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(interval));
     }    
 }
